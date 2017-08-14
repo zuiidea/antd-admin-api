736945c362f1e7fa757f34c3269a4ad106e4e18e
zuiidea <zuiiidea@gmail.com>
2017-08-14 11:06:23 +0800

    first commit

---
+++ .roadhogrc.js
@@ -0,0 +1,10 @@
+export default {
+  entry : 'src/index.js',
+  "proxy": {
+    "/api/v1/weather": {
+      "target": "https://api.seniverse.com/",
+      "changeOrigin": true,
+      "pathRewrite": { "^/api/v1/weather" : "/v3/weather" }
+    },
+  },
+}
---
+++ README.md
@@ -0,0 +1,3 @@
+### antd-admin-api
+
+For [antd-admin](https://github.com/zuiidea/antd-admin) as mock server 
